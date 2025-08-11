839db90d34ea63a84143410ac6b2efc3a007c9d8
SIRABAZA <omarabaza670@gmail.com>
2025-08-11 13:21:04 +0300

    install tailwind and init shadcn in project

---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Button } from "@/components/ui/button";
+
+export default function Login() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Login</h1>
+      <Button>Login</Button>
+    </div>
+  );
+}
